17c598d25e294c4b103f1401822741c290b8397a
natesway <64743334+natesway@users.noreply.github.com>
2023-08-03 09:35:08 -0400

    Update PauseSubState.hx

--- source/substates/PauseSubState.hx
+++ source/substates/PauseSubState.hx
@@ -274,9 +274,9 @@ class PauseSubState extends MusicBeatSubstate
 				case 'Toggle Botplay':
 					PlayState.instance.cpuControlled = !PlayState.instance.cpuControlled;
 					PlayState.changedDifficulty = true;
-					PlayState.instance.botplayTxt.visible = PlayState.instance.cpuControlled;
-					PlayState.instance.botplayTxt.alpha = 1;
-					PlayState.instance.botplaySine = 0;
+					//PlayState.instance.botplayTxt.visible = PlayState.instance.cpuControlled;
+					//PlayState.instance.botplayTxt.alpha = 1;
+					//PlayState.instance.botplaySine = 0;
 				case 'Options':
 					PlayState.instance.paused = true; // For lua
 					PlayState.instance.vocals.volume = 0;
