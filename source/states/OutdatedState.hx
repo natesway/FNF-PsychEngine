416cb27054013e6c352a53ff62aae617612fe8c6
natesway <64743334+natesway@users.noreply.github.com>
2023-08-03 22:24:27 -0400

    Update OutdatedState.hx

--- source/states/OutdatedState.hx
+++ source/states/OutdatedState.hx
@@ -13,7 +13,7 @@ class OutdatedState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Sup bro, looks like you're running an   \n
+			"Hey um, looks like you're running an   \n
 			outdated version of The mod (" + MainMenuState.psychEngineVersion + "),\n
 			please update to " + TitleState.updateVersion + "!\n
 			Press ESCAPE to proceed anyway.\n
