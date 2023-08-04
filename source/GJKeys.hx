09998ea882a1ae4825b55bbfe7a3041858ebc76f
natesway <64743334+natesway@users.noreply.github.com>
2023-08-03 23:10:44 -0400

    Create GJKeys.hx

---
+++ source/GJKeys.hx
@@ -0,0 +1,19 @@
+package;
+
+/**
+ * YOU MUST NOT SHARE THESE DATA WITH ANYONE!!
+ * 
+ * 
+ * The publication of these data would lead to hacking or other kinds of alterations in your mod!
+ * To solve this problem, you must add the following line to the `.gitignore` file in the main directory
+ * ```gitignore
+ * source/gamejolt/GJKeys.hx
+ * ```
+ */
+class GJKeys {
+	/** Your Game ID from GameJolt **/
+	public static var id:Int = 0;
+
+	/** Your Game Private Key from GameJolt **/
+	public static var key:String = '';
+}
