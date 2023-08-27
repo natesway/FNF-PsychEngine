edd1083d24d2dba2ecea0ff6ab96eaea5e58b937
natesway <64743334+natesway@users.noreply.github.com>
2023-08-27 13:47:13 -0400

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -55,7 +55,10 @@ import sys.io.File;
 #end
 
 #if VIDEOS_ALLOWED 
-import hxcodec.flixel.FlxVideo as VideoHandler;
+#if (hxCodec >= "3.0.0") import hxcodec.flixel.FlxVideo as VideoHandler;
+#elseif (hxCodec >= "2.6.1") import hxcodec.VideoHandler as VideoHandler;
+#elseif (hxCodec == "2.6.0") import VideoHandler;
+#else import vlc.MP4Handler as VideoHandler; #end
 #end
 
 import objects.Note.EventNote;
