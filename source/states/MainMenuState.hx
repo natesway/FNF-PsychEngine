123d80d39daea2ce1bdd1494c2487ddef5681dfa
natesway <64743334+natesway@users.noreply.github.com>
2023-09-09 01:24:20 -0400

    Final touches

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -121,7 +121,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollow, null, 0);
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Shattered Engine v" + shatteredEngineVersion, 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "Shattered Engine v" + shatteredEngineVersion, 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
